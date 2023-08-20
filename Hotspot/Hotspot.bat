479a84cf360741fefe64a1d4699756ef909ab909
Gaurav Mittal <44697444+Gauravmittal16@users.noreply.github.com>
2023-08-20 17:42:33 +0530

    hotspot

---
+++ Hotspot/Hotspot.bat
@@ -0,0 +1,2 @@
+netsh wlan set hostednetwork mode=allow ssid=gaurav key=password
+netsh wlan start hostednetwork 
---
+++ Hotspot/start.bat
@@ -0,0 +1,3 @@
+netsh wlan set hostednetwork mode=allow ssid=Gaurav key=trojan321 
+netsh wlan start hostednetwork
+pause
