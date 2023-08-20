12f89a585560a03860ac2cdf9a7409eb7cc08f83
Gaurav Mittal <44697444+Gauravmittal16@users.noreply.github.com>
2023-08-20 17:51:49 +0530

    bootstrap

---
+++ validation.php
@@ -0,0 +1,15 @@
+<?php
+$name=$_POST['username'];
+$uspass=$_POST['userpass'];
+$adpass=$_POST['adpass'];
+$connection=mysqli_connect("localhost","root","8755");
+if(name!=0)
+{
+	
+}
+else
+{
+	
+}
+
+?>
